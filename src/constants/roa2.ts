4a7561614568587a4791b710112c81e64fa67229
Leonard Dietrich <dietrich.leonard@googlemail.com>
2026-07-05 17:43:44 +0200

    add slade + remove unused character and maps

--- src/constants/roa2.ts
+++ src/constants/roa2.ts
@@ -5,22 +5,18 @@
 // Import all character portraits
 import AbsaPortrait from '@/assets/roa2/characters/RoA2_Absa_Portrait.png';
 import ClairenPortrait from '@/assets/roa2/characters/RoA2_Clairen_Portrait.png';
-// import EllianaPortrait from '@/assets/roa2/characters/RoA2_Elliana_Portrait.png';
 import EtalusPortrait from '@/assets/roa2/characters/RoA2_Etalus_Portrait.png';
 import FleetPortrait from '@/assets/roa2/characters/RoA2_Fleet_Portrait.png';
 import ForsburnPortrait from '@/assets/roa2/characters/RoA2_Forsburn_Portrait.png';
 import GalvanPortrait from '@/assets/roa2/characters/RoA2_Galvan_Portrait.png';
-// import HodanPortrait from '@/assets/roa2/characters/RoA2_Hodan_Portrait.png';
 import KraggPortrait from '@/assets/roa2/characters/RoA2_Kragg_Portrait.png';
 import LaReinaPortrait from '@/assets/roa2/characters/RoA2_La_Reina_Portrait.png';
 import LoxodontPortrait from '@/assets/roa2/characters/RoA2_Loxodont_Portrait.png';
 import MaypulPortrait from '@/assets/roa2/characters/RoA2_Maypul_Portrait.png';
-// import MolloPortrait from '@/assets/roa2/characters/RoA2_Mollo_Portrait.png';
 import OlympiaPortrait from '@/assets/roa2/characters/RoA2_Olympia_Portrait.png';
 import OrcanePortrait from '@/assets/roa2/characters/RoA2_Orcane_Portrait.png';
-// import PommePortrait from '@/assets/roa2/characters/RoA2_Pomme_Portrait.png';
 import RannoPortrait from '@/assets/roa2/characters/RoA2_Ranno_Portrait.png';
-// import SylvanosPortrait from '@/assets/roa2/characters/RoA2_Sylvanos_Portrait.png';
+import SladePortrait from '@assets/roa2/characters/RoA2_Slade_Portrait.png';
 import WrastorPortrait from '@/assets/roa2/characters/RoA2_Wrastor_Portrait.png';
 import ZetterburnPortrait from '@/assets/roa2/characters/RoA2_Zetterburn_Portrait.png';
 
@@ -29,28 +25,24 @@ import ZetterburnPortrait from '@/assets/roa2/characters/RoA2_Zetterburn_Portrai
 import AetherianForestStagePng from '@/assets/roa2/stages/AetherianForest.png';
 import AirArmadaStagePng from '@/assets/roa2/stages/AirArmada.png';
 import FireCapitalStagePng from '@/assets/roa2/stages/FireCapital.png';
-import ForestUnderstoryStagePng from '@/assets/roa2/stages/ForestUnderstory.png';
 import GodaiDeltaStagePng from '@/assets/roa2/stages/GodaiDelta.png';
 import HodojoStagePng from '@/assets/roa2/stages/Hodojo.png';
 import HyperboreanHarborStagePng from '@/assets/roa2/stages/HyperboreanHarbor.png';
 import JulesvaleStagePng from '@/assets/roa2/stages/Julesvale.png';
 import MerchantPortStagePng from '@/assets/roa2/stages/MerchantPort.png';
 import RockWallStagePng from '@/assets/roa2/stages/RockWall.png';
-import StormsweptPillarStagePng from '@/assets/roa2/stages/StormsweptPillar.png';
 import TempestPeakStagePng from '@/assets/roa2/stages/TempestPeak.png';
 
 // Stage WEBP preferred assets
 import AetherianForestStageWebp from '@/assets/roa2/stages/AetherianForest.webp';
 import AirArmadaStageWebp from '@/assets/roa2/stages/AirArmada.webp';
 import FireCapitalStageWebp from '@/assets/roa2/stages/FireCapital.webp';
-import ForestUnderstoryStageWebp from '@/assets/roa2/stages/ForestUnderstory.webp';
 import GodaiDeltaStageWebp from '@/assets/roa2/stages/GodaiDelta.webp';
 import HodojoStageWebp from '@/assets/roa2/stages/Hodojo.webp';
 import HyperboreanHarborStageWebp from '@/assets/roa2/stages/HyperboreanHarbor.webp';
 import JulesvaleStageWebp from '@/assets/roa2/stages/Julesvale.webp';
 import MerchantPortStageWebp from '@/assets/roa2/stages/MerchantPort.webp';
 import RockWallStageWebp from '@/assets/roa2/stages/RockWall.webp';
-import StormsweptPillarStageWebp from '@/assets/roa2/stages/StormsweptPillar.webp';
 import TempestPeakStageWebp from '@/assets/roa2/stages/TempestPeak.webp';
 
 import type { Rival, Stage, RivalElement } from '@/types/roa2Types';
@@ -150,6 +142,12 @@ export const RIVALS: Rival[] = [
 		iconPath: EtalusPortrait,
 		color: '#FFA500',
 	},
+	{
+		name: 'Slade',
+		element: 'Water',
+		iconPath: SladePortrait,
+		color: '#1E90FF',
+	}
 	// {
 	// 	name: 'Sylvanos',
 	// 	element: 'Earth',
@@ -208,14 +206,13 @@ export const STAGES: Stage[] = [
 		iconPathWebp: JulesvaleStageWebp,
 	},
 	{
-		name: 'Merchant Port',
-		iconPath: MerchantPortStagePng,
-		iconPathWebp: MerchantPortStageWebp,
-	},
-	{
 		name: 'Air Armada',
 		iconPath: AirArmadaStagePng,
 		iconPathWebp: AirArmadaStageWebp,
+	},	{
+		name: 'Merchant Port',
+		iconPath: MerchantPortStagePng,
+		iconPathWebp: MerchantPortStageWebp,
 	},
 	{
 		name: 'Fire Capital',
@@ -236,17 +233,7 @@ export const STAGES: Stage[] = [
 		name: 'Tempest Peak',
 		iconPath: TempestPeakStagePng,
 		iconPathWebp: TempestPeakStageWebp,
-	},
-	{
-		name: 'Forest Understory',
-		iconPath: ForestUnderstoryStagePng,
-		iconPathWebp: ForestUnderstoryStageWebp,
-	},
-	{
-		name: 'Stormswept Pillar',
-		iconPath: StormsweptPillarStagePng,
-		iconPathWebp: StormsweptPillarStageWebp,
-	},
+	}
 ]
 
 /** List of all rival elements */
